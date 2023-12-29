17ba88ac73e8c13f42b34e44db1057ad4a3913e5
TomiokaGiuy <polienko.vlad@list.ru>
2023-12-29 17:32:49 +0300

    First commit

---
+++ App2/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var page = new NavigationPage( new MainPage());
+            page.BarBackgroundColor = Color.Red;
+            MainPage = page;
+        }
+
+        protected override void OnStart()
+        {
+            // Проверка, нужно ли открывать страницу авторизации при запуске приложения
+            if (!IsLoggedIn())
+            {
+                MainPage.Navigation.PushModalAsync(new NavigationPage(new LoginPage()));
+            }
+        }
+
+        private bool IsLoggedIn()
+        {
+            // Ваша логика проверки состояния авторизации
+            return false;
+        }
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App2/LoginPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using App2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        // Ваша логика страницы LoginPage
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            // Ваша логика для обработки события нажатия на кнопку входа
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            if (CheckCredentials(username, password))
+            {
+                // Если учетные данные верны, перейти на главную страницу
+                MainPage mainPage = new MainPage();
+                NavigationPage.SetHasNavigationBar(mainPage, false);
+                Navigation.PushAsync(mainPage);
+            }
+            else
+            {
+                // Если учетные данные неверны, показать сообщение об ошибке
+                DisplayAlert("Ошибка", "Неверное имя пользователя или пароль", "OK");
+            }
+        }
+
+        // Метод для проверки учетных данных
+        private bool CheckCredentials(string username, string password)
+        {
+            // Ваша логика для проверки учетных данных
+            // Здесь вы можете выполнить аутентификацию с помощью API, базы данных и т.д.
+            return (username == "admin" && password == "password");
+        }
+
+        private async void AddRegisterPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrPage());
+        }
+    }
+}
